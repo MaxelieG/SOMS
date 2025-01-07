519c27d5f288d212f8440b58478ec2973908df32
Maxelie Gilbert <maxelie.gilbert@student-cs.fr>
2025-01-07 14:53:37 +0100

    new

---
+++ fiona.py
@@ -0,0 +1,4 @@
+#Pour le problème de fiona, j'ai trouvé une solution ce Week-end, 
+#c'est possible d'utiliser le module pyogrio à la place en ouvrant les fichiers, 
+#comme ça par exemple : 
+# France_entier = gpd.read_file('C:/Users/lecom/Documents/Master_EEET/SOMS_Case_studies/contour_France/all_france_dissolve.shp', engine='pyogrio')
